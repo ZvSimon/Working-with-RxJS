2d2c7d87decccb6a612ecc5c3165e10c7e7995e9
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-09-01 02:33:42 +0200

    Creating & Using an Observable : interval RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit{
+  private destroyRef = inject(DestroyRef)
+ngOnInit() {
+  const subscription=interval(1000).subscribe( {
+    next:(value: number) => console.log(value),
+    error: (error: any) => console.error(error),
+  });
+    this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+    });
+}
 }
