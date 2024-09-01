c62421335d764a037a28635b3cb615b869e27991
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-09-01 14:23:36 +0200

    Converting Signal To Observables with toObservable from rxjs-interop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component, computed, effect, OnInit, signal} from '@angular/core';
+import {Component, computed, DestroyRef, effect, inject, OnInit, signal} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {toObservable} from "@angular/core/rxjs-interop";
 
 
 @Component({
@@ -12,21 +13,23 @@ import {NgOptimizedImage} from "@angular/common";
 })
 export class AppComponent implements OnInit{
   clickCount= signal(0);
-  interval = signal(0);
-  doubleInterval = computed(() => this.interval() * 2);
-  // private destroyRef = inject(DestroyRef);
+  clickCount$= toObservable(this.clickCount);
+  // interval = signal(0);
+  // doubleInterval = computed(() => this.interval() * 2);
+  private destroyRef = inject(DestroyRef);
   constructor() {
-    effect(() => {
-      console.log(`Clicked button ${this.clickCount()} times`);
-    });
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times`);
+    // });
+    // toObservable(this.clickCount);
   }
   ngOnInit() {
 
-    setInterval(() => {
-        this.interval.update((prevValue) => prevValue + 1);
-        console.log("interval", this.interval());
-        console.log("doubleInterval", this.doubleInterval());
-    },1000);
+    // setInterval(() => {
+    //     this.interval.update((prevValue) => prevValue + 1);
+    //     console.log("interval", this.interval());
+    //     console.log("doubleInterval", this.doubleInterval());
+    // },1000);
   // const subscription=interval(1000).pipe( map((value)=>value*2)).subscribe(
   // {
   //   next:(value: number) => console.log(value),
@@ -35,6 +38,12 @@ export class AppComponent implements OnInit{
   //   this.destroyRef.onDestroy(() => {
   //       subscription.unsubscribe();
   //   });
+    const subscription = this.clickCount$.subscribe( {
+      next:(value: number) => console.log(`Clicked button ${this.clickCount()} times`)
+    });
+    this.destroyRef.onDestroy(() => {
+            subscription.unsubscribe();
+        });
   }
   onClick() {
     this.clickCount.update((prevCount) => prevCount + 1);
