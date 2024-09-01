39424b8436fdc191c98e960bca63ec80c3b1f45d
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-09-01 14:11:52 +0200

    Delete observables and using signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,42 @@
-import {Component, DestroyRef, inject, OnInit} from '@angular/core';
-import {interval} from "rxjs";
+import {Component, computed, effect, OnInit, signal} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit{
-  private destroyRef = inject(DestroyRef)
-ngOnInit() {
-  const subscription=interval(1000).subscribe( {
-    next:(value: number) => console.log(value),
-    error: (error: any) => console.error(error),
-  });
-    this.destroyRef.onDestroy(() => {
-        subscription.unsubscribe();
+  clickCount= signal(0);
+  interval = signal(0);
+  doubleInterval = computed(() => this.interval() * 2);
+  // private destroyRef = inject(DestroyRef);
+  constructor() {
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times`);
     });
-}
+  }
+  ngOnInit() {
+
+    setInterval(() => {
+        this.interval.update((prevValue) => prevValue + 1);
+        console.log("interval", this.interval());
+        console.log("doubleInterval", this.doubleInterval());
+    },1000);
+  // const subscription=interval(1000).pipe( map((value)=>value*2)).subscribe(
+  // {
+  //   next:(value: number) => console.log(value),
+  //   error: (error: any) => console.error(error),
+  // });
+  //   this.destroyRef.onDestroy(() => {
+  //       subscription.unsubscribe();
+  //   });
+  }
+  onClick() {
+    this.clickCount.update((prevCount) => prevCount + 1);
+  }
 }
